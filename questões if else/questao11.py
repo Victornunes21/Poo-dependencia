877344f1dd458a863c9ee2a86bbedbdeed82dbd5
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-06-22 11:46:35 -0300

    if

---
+++ questões if else/questao11.py
@@ -0,0 +1,13 @@
+print("Vamos calcular o seu IMC")
+peso = float(input("manda ai o teu peso: "))
+altura = float(input("manda também a tua altura: "))
+IMC = peso / (altura **2)
+print("O seu imc é {:.2f}".format(IMC))
+if IMC < 18.5:
+    print("Bixu, tu tá abaixo do peso!")
+elif IMC == 18.5 or IMC < 25.5:
+    print("Você esta com o peso normal!")
+elif IMC == 25.5 or IMC <= 30:
+    print("Bixu, toma cuidado! você esta acima do peso!")
+elif IMC > 30:
+    print("Boy! para de comer tu já ta obeso!")
