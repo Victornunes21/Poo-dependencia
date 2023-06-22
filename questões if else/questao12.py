4c084e7f179bf5dcf4a25c547e7c95b7a99679ef
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-06-22 11:15:33 -0300

    if

---
+++ questões if else/questao12.py
@@ -0,0 +1,27 @@
+mes = int(input("Qual o mês do seu nascimento? "))
+if mes == 1:
+    print("Você nasceu no mês de Janeiro!")
+if mes == 2:
+    print("Você nasceu no mês de Fevereiro!")
+if mes == 3:
+    print("Você nasceu no mês de Março!")
+if mes == 4:
+    print("Você nasceu no mês de Maio!")
+if mes == 5:
+    print("Você nasceu no mês de Abril!")
+if mes == 6:
+    print("Você nasceu no mês de Junho!")
+if mes == 7:
+    print("Você nasceu no mês de Julho!")
+if mes == 8:
+    print("Você nasceu no mês de Agosto!")
+if mes == 9:
+    print("Você nasceu no mês de Setembro!")
+if mes == 10:
+    print("Você nasceu no mês de Outubro!")
+if mes == 11:
+    print("Você nasceu no mês de Novembro!")
+if mes == 12:
+    print("Você nasceu no mês de Dezembro!")
+elif mes > 12:
+    print("Boy! Esse mês não existe!")
