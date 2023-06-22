0f5a283ab9c5eea7d394f0dd33606a5456ef2805
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-06-22 11:00:28 -0300

    if

---
+++ questões if else/questao8.py
@@ -0,0 +1,15 @@
+idade = int(input("Qual a idade da criança: "))
+if idade == 5 or idade == 6 or idade == 7:
+    print("Sua categoria é a Infatil A!")
+if idade == 8 or idade == 9 or idade == 10:
+    print("Sua categoria é a Infantil B!")
+if idade == 11 or idade == 12 or idade == 13:
+    print("Sua categoria é Juvenil A!")
+if idade == 14 or idade == 15 or idade == 16 or idade == 17:
+    print("Sua categoria é Juvenil B!")
+else:
+    print("Sua categoria é Adulto!")
+
+  
+
+
