57582ac6f096c0a4472f9b935ac153807dcdc9e6
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-06-23 12:54:30 -0300

    if

---
+++ questões if else/questao9.py
@@ -0,0 +1,13 @@
+preco = float(input("brothe, quanto foi esse produto: "))
+pagam = int(input("broth, qual vai ser a forma de pagamento de compra: "))
+if pagam == 1:
+    npreco = preco - (preco * 10 / 100)
+    print("O novo valor do produto é de", npreco)
+elif pagam == 2:
+    precon = preco - (preco * 5/ 100)
+    print("O novo preço é de ", precon)
+elif pagam == 3:
+    print("Brothe, dessa forma o preço continua o mesmo!")
+elif pagam == 4:
+    novop = preco + (preco * 10/100)
+    print("Brthe, o preço deu uma aumentada, agora custa", novop)
