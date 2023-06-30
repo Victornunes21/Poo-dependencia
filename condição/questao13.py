74776da7e6c90c0e95ec1874ce275b21c9c459da
vivioliver <122638547+vivioliver@users.noreply.github.com>
2023-06-30 13:00:55 -0300

    if

---
+++ condição/questao13.py
@@ -0,0 +1,7 @@
+dia = int(input("Informe o dia: "))
+mes = int(input("Informe o mês: "))
+ano = int(input("Informe o ano: "))
+if dia > 31 or mes > 13:
+    print("Data Inválida!")
+else:
+    print("Data Válida")
