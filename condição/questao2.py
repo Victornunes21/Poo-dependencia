8356b565cb6e6593d4b0709b68cc14529737426b
vivioliver <122638547+vivioliver@users.noreply.github.com>
2023-06-30 12:48:50 -0300

    if

---
+++ condição/questao2.py
@@ -0,0 +1,16 @@
+n1 = float(input('Escreva um número: '))
+n2 = float(input('Escreva um número: '))
+n3 = float(input('Escreva um número: '))
+
+if n1 >= n2 and n1 >= n3 and n2 >= n3:
+    print(f'A ordem decrescente é {n1} , {n2} e {n3}')
+elif n1 >= n2 and n1 >= n3 and n3 >= n2:
+    print(f'A ordem decrescente é {n1} , {n3} e {n2}')
+elif n2 >= n1 and n2 >= n3 and n1 >= n3:
+    print(f'A ordem decrescente é {n2} , {n1} e {n3}')
+elif n2 >= n1 and n2 >= n3 and n3 >= n1:
+    print(f'A ordem decrescente é {n2} , {n3} e {n1}')
+elif n3 >= n1 and n3 >= n2 and n1 >=n2:
+    print(f'A ordem decrescente é {n3} , {n1} e {n2}')
+elif n3 >= n1 and n3 >= n2 and n2 >= n1:
+    print(f'A ordem decrescente é {n3} , {n2} e {n1}')
