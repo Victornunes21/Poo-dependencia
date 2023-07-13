48616088d5050d5432c3f97a97d215a68b5e047c
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-07-13 11:30:43 -0300

    sequencia

---
+++ senquência/exemplo.py
@@ -0,0 +1,5 @@
+palavra = str(input("Manda ai uma palavra: "))
+repet = int(input("Quantas vezes ela vai se repetir: "))
+for i in range(repet):
+    print(palavra)
+
---
+++ senquência/questão21.py
@@ -0,0 +1,11 @@
+cont = 0
+acum = 0
+numero = int(input("Digite um número:"))
+for i in range(1,10):
+    novon = int(input("Manda ai uns numeros: "))
+    acum = acum + novon
+    soma = acum
+    media = soma / 10
+
+print(soma)
+print(media)
