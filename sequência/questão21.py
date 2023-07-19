08471fd74043ab3ed99ece609a72af13eba22a58
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-07-19 12:48:04 -0300

    ..

--- sequência/questão21.py
+++ sequência/questão21.py
@@ -1,11 +1,24 @@
 cont = 0
 acum = 0
-numero = int(input("Digite um número:"))
-for i in range(1,10):
-    novon = int(input("Manda ai uns numeros: "))
-    acum = acum + novon
-    soma = acum
-    media = soma / 10
+maiorn = 0
+menorn = 500000
+for i in range(1,11):
+    numero = int(input("Manda ai uns numeros: "))
+    acum = acum + numero
+    media = acum / 10
+    if numero > maiorn:
+        maiorn = numero
+        if numero < menorn:
+            menorn = numero
+        else:
+            continue
+    else:
+        continue
+    
 
-print(soma)
-print(media)
+    
+
+print(f"A soma de todos os números é igual á {acum}")
+print(f"A média dos números digitados é igual á {media}")
+print(f"O maior número digitados foi {maiorn}")
+print(f"O menor número digitado foi {menorn}")
