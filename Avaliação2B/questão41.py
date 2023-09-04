9169516668adfdc011cd10e4d43fcab0ca0e70d0
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-09-04 10:44:58 -0300

    .

--- Avaliação2B/questão41.py
+++ Avaliação2B/questão41.py
@@ -1,18 +1,9 @@
 
-"""acm = 0
+acm = 0
 for i in range(1,11):
     idade = int(input("Manda ai 10 idade: "))
     acm += idade
     meida = acm/10
-print(f"A media das idade digitadas foram de {meida}")"""
-acm = 0
-cont = 0
-idade = int(input("digite uma idade: "))
-while idade != 0:
-    acm += idade
-    cont += 1
-    idade = int(input("Manda ai as idade: "))
+print(f"A media das idade digitadas foram de {meida}")
 
-media = acm / cont
-print(f"A media das idades digitadas foram de {media}")
     
