98238fdbeb4df55b944f4875a76056481a47c0a1
J.victt <102590669+Victornunes21@users.noreply.github.com>
2023-10-10 11:46:00 -0300

    IF

---
+++ manipulação de arquivos/exercicio/ex.py
@@ -0,0 +1,15 @@
+"""import os
+os.getcwd()
+
+os.chdir("D:/Users/20201174010005/Documents/GitHub/Poo-dependencia/manipulação de arquivos/exercicio")
+os.getcwd()
+
+
+dados = open ("D:/Users/20201174010005/Documents/GitHub/Poo-dependencia/manipulação de arquivos/exercicio/pergunta/dialogo.txt")
+print(dados.readline, end="")"""
+
+pessoas = 2
+for i in range(pessoas):
+    nome = input("What your name: ")
+print(nome)
+
